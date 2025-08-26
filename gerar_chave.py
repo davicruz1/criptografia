5840ac044fe85df3b108a942bb05e32b5240aae5
davicruz1 <davicruzramos1@gmail.com>
2025-08-26 17:19:38 -0300

    gerar_chave.py

---
+++ gerar_chave.py
@@ -0,0 +1,8 @@
+from cryptography.fernet import Fernet
+
+def gerar_chave():
+    chave = Fernet.generate_key()
+    with open("chave.key", "wb") as chave_arquivo:
+        chave_arquivo.write(chave)
+
+gerar_chave()
